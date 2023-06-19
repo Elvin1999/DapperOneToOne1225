1298a019f24ba47fb4071f46d26dbb4c45810c4b
camalzadeelvin <camalzadeelvin@STHQ0118-01>
2023-06-19 10:56:31 +0400

    Add project files.

---
+++ DapperOneToOne/Entities/Country.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperOneToOne.Entities
+{
+    public class Country
+    {
+        public int CountryId { get; set; }
+        public string Name { get; set; }
+        public int CapitalId { get; set; }
+        public virtual Capital Capital { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
